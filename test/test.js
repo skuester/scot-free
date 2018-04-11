b3f9ac3dba17908fb95d54251ba8da05d7b7dd83
Shane Kuester <shout@shanekuester.com>
2018-04-11 01:04:44 -0500

    flesh out some basic examples
    For now, I’m just prototyping the design of the markup itself. Eventually, I want to implement the final version of this library using streams. Simple arrays will do for now until the design is more mature.

--- test/test.js
+++ test/test.js
@@ -1,10 +1,63 @@
 // TODO: check out tap-difflet
 
 const test = require('tape')
+const ScotFree = require('./../lib/scot-free')
+
+// WANT
+// template.pipe(ScotFree.stream(opts)).pipe(out_file)
+
+example(
+'a paragraph',
+
+`Hello, World`,
+
+`<p>Hello, World</p>`
+)
 
 
 
-test('it works', t => {
-	t.pass('Yay?')
-	t.end()
-})
+example(
+'a multiline block',
+
+`
+one
+two
+	three
+`,
+
+`<pre>one
+two
+	three</pre>`
+)
+
+
+example(
+'an indented paragraph',
+
+`
+this is a normal paragraph
+
+	this paragraph is indented 1
+
+		this paragraph is indented 2
+`,
+
+`<p>this is a normal paragraph</p>
+<p class="t1">this paragraph is indented 1</p>
+<p class="t2">this paragraph is indented 2</p>`
+)
+
+
+
+
+
+
+
+
+function example(description, template, output) {
+	test(description, t => {
+		let result = ScotFree(template)()
+		t.equal(result, output, 'template is correct')
+		t.end()
+	})
+}
